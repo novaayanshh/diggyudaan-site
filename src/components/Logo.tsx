2a191e5c57fbc2cb0dd0ab4be8dd2190b9598121
Ayansh Pandey <ayanshpandey555@gmail.com>
2026-07-22 22:55:26 +0530

    Increase logo size and update navbar

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -11,7 +11,7 @@ export function Logo({ className = "" }: { className?: string }) {
       <img
         src="/logo.png"
         alt="DigyUdaan Media House - Let's Fly Together"
-        className="h-10 w-auto md:h-12"
+        className="h-14 w-auto md:h-16 lg:h-20 object-contain"
       />
     </motion.div>
   );
